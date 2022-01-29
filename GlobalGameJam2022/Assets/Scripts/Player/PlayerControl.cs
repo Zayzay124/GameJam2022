c3ac6a20a935b99bd6f3d94c179ab74a42dedc2b
Isaiah Rettig <isaiah.rettig@icloud.com>
2022-01-29 16:23:48 -0500

    Player added
    I programmed a prototype player that can jump and land on platforms. Currently the player can double jump in the air which is something I don't want. Code is not yet commented

---
+++ GlobalGameJam2022/Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    //private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask groundLayer;
+
+    private int extraJumps;
+    public int extraJumpsValue;
+
+    Vector2 origPos;
+
+    void Start()
+    {
+        origPos = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+        extraJumps = extraJumpsValue;
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    void Update()
+    {
+       if(isGrounded == true) {
+            extraJumps = extraJumpsValue;
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0) {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0) {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+}
